82b7343b143c5fa26f7fb3140ef7d06f9baebed3
Kunal Kankariya <kkankariya007@gmail.com>
2023-03-02 14:33:32 +0530

    Hey

---
+++ Procfile
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+=======
+web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+>>>>>>> 039c02266497662c03dde1acf9a08a91c326fdd9
---
+++ exp4/e8c.c
@@ -0,0 +1,37 @@
+//FTP client
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<stdio.h>
+#include<unistd.h>
+#include <string.h>
+int main(int argc,char *argv[])
+{
+int sd,cd;
+struct sockaddr_in servaddr,cliaddr;
+socklen_t clilen;
+char buff[100],file[10000];
+struct hostent *h;
+h=gethostbyname(argv[1]);
+bzero(&servaddr,sizeof(servaddr));
+servaddr.sin_family=h->h_addrtype;
+memcpy((char *)&servaddr.sin_addr.s_addr,h->h_addr_list[0],h->h_length);
+servaddr.sin_port=htons(9500);
+sd=socket(AF_INET,SOCK_STREAM,0);
+cd=connect(sd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+while(1)
+{
+printf("%s\n","\nEnter the File Name :");
+scanf("%s",buff);
+
+send(sd,buff,strlen(buff)+1,0);
+printf("%s\n","File Output :");
+if(recv(sd,file,sizeof(file),0))printf("%s",file);
+else{
+    printf("File does not exist!");
+}
+}
+return 0;
+}
